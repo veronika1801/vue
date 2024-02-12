1c83de4ead57ad957e2ea5ab5f09dacd49d7f3b3
smval18 <chikibriki1801@gmail.com>
2024-02-12 20:23:28 +0700

    5 задание

--- js/main.js
+++ js/main.js
@@ -11,21 +11,37 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "../assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "../assets/vmSocks-blue-onWhite.jpg",
             }
-         ],
-         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-         
-         
-    }
- })
- 
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        deleteFromCart() {
+            this.cart -= 1
+            if (this.cart < 0) {
+                this.cart = 0
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+    },
 
- 
- 
+})
 
- 
+
+
+
+
+
