a8ca9cad544aa580cc2d68ccad47531ca6e08ae5
smval18 <chikibriki1801@gmail.com>
2024-02-12 18:25:22 +0700

    ..

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks"
+    }
+ })
+ let desc = new Vue({
+    el: '#desc',
+    data: {
+        description: "A pair of warm, fuzzy socks"
+    }
+ })
+ 
 
  
