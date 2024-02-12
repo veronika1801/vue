a64bfebf0bfe8102fc5229c945a5c3017a4165d0
smval18 <chikibriki1801@gmail.com>
2024-02-12 19:24:58 +0700

    4 задание

--- js/main.js
+++ js/main.js
@@ -7,6 +7,20 @@ let app = new Vue({
         inStock: true,
         inventory: 100,
         onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+         ],
+         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+         
+         
     }
  })
  
